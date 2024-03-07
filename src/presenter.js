86d9c263df76c57215d7d848d1c919b88a312cbf
Salma Torres <tsalma569@gmail.com>
2024-03-07 01:08:14 -0400

    Se ordenaron los mensajes sobre los impuestos y sobre los descuentos

--- src/presenter.js
+++ src/presenter.js
@@ -35,33 +35,20 @@ form.addEventListener("submit", (event) => {
   const descuentoEnvio_aplicadoTotal = descuentoEnvio_aplicado * cantidad;
   const precioTotal = precioNeto + impuestoAplicado - descuentoAplicado + impuesto_aplicadoProducto - descuentoAplicadoCategoria + costoEnvioTotal - descuentoEnvio_aplicadoTotal - descuento_fijo;
 
-  // Crea un elemento de párrafo
-  const paragraphPrecioNeto = document.createElement("p");
-  paragraphPrecioNeto.textContent = `Precio neto (${cantidad} * ${precio}): $${precioNeto}`;
-
-  const paragraphImpuesto = document.createElement("p");
-  paragraphImpuesto.textContent = `Impuesto para ${estado}(${impuesto}%):$${impuestoAplicado}`;
-
-  const paragraphDescuentoCategoria = document.createElement("p");
-  paragraphDescuentoCategoria.textContent = `Descuento para ${categoria} (${descuento_adicionalProducto}%): $${descuentoAplicadoCategoria}`;
-
-  const paragraphPesoVolumetrico = document.createElement("p");
-  paragraphPesoVolumetrico.textContent = `Costo de Envío: $${costoenvio}`;
-
   // Limpia el contenido anterior de div
   div.innerHTML = "";
 
   // Agrega el párrafo al div
+  var m_PrecioNeto = "<p>Precio neto (" + cantidad + "*" + "$" + precio + "): $" + precioNeto + "</p>";
+  var m_impuesto = "<p>Impuesto para " + estado + "(" + impuesto + "%): $" + impuestoAplicado + "</p>";
   var mensaje_descuento = "<p> Descuento (" + descuento + "%): $"+ descuentoAplicado + "<p/>";
   var mensaje_impuestoProducto = "<p> Impuesto para " + categoria + "(" + impuesto_adicionalProducto + "%): $" + impuesto_aplicadoProducto +"<p/>";
+  var m_descuentoCategoria = "<p> Descuento para " + categoria + "(" + descuento_adicionalProducto + "%): $" + descuentoAplicadoCategoria + "<p/>"
   var mensaje_precioTotal = "<p> Precio Total (descuentos e impuestos): $" + precioTotal + "</p>";
+  var m_pesoVolumetrico = "<p> Costo de envio por unidad: $" + costoenvio + "</p>";
   var mensaje_costoEnvioTotal = "<p> Costo de Envio Total ($" + costoenvio + " * " + cantidad + "): " + costoEnvioTotal + "</p>";
   var mensaje_descuentoEnvio = "<p> Descuento de costo de envio para cliente " + tipocli + "(" + descuento_Envio + "%): $" + descuentoEnvio_aplicado + "</p>";
   var mensaje_descuentoEnvioTotal = "<p> Descuento de costo de envio total  para cliente " + tipocli + "(" + descuento_Envio + "%): $" + descuentoEnvio_aplicadoTotal + "</p>";
   var mensaje_descuentoFijo = "<p> Descuento fijo (tipo cliente: " + tipocli + ", precio neto: $" + precioNeto + ", categoria producto" + categoria + "): $" + descuento_fijo + "</p>";
-  div.innerHTML = mensaje_descuento + mensaje_impuestoProducto + mensaje_costoEnvioTotal + mensaje_descuentoEnvio + mensaje_descuentoEnvioTotal + mensaje_descuentoFijo + mensaje_precioTotal;
-  div.appendChild(paragraphPrecioNeto);
-  div.appendChild(paragraphImpuesto);
-  div.appendChild(paragraphDescuentoCategoria);
-  div.appendChild(paragraphPesoVolumetrico);
+  div.innerHTML = m_PrecioNeto + m_impuesto + mensaje_descuento + mensaje_impuestoProducto + m_descuentoCategoria + m_pesoVolumetrico + mensaje_costoEnvioTotal + mensaje_descuentoEnvio + mensaje_descuentoEnvioTotal + mensaje_descuentoFijo + mensaje_precioTotal;
 });
